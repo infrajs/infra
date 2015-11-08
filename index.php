f4755db8b59f9453a36b062d9488c5044d053f43
Антон Кияткин <aky@list.ru>
2015-11-08 11:52:53 +0400

    .htaccess prefer

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ require_once('vendor/autoload.php');
 require_once('infra.php');
 $conf=infra_config();
 
-itlife\infrajs\Infrajs::controller($conf['infra']['controller']);
+itlife\infrajs\Infrajs::controller($conf['controller']);
